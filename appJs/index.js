cfdb0fc1a43266772afd7ad3ee43b7fe2fb3ee3c
teresamorini <teresa.morini91@gmail.com>
2023-02-27 16:36:33 +0100

    env and config map

--- appJs/index.js
+++ appJs/index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('Hello from Node.js!');
-    //res.send('Hello ' + process.env.USER_ID + ' from Node.js!');
+})
+
+app.get('/hello/v1', (req, res) => {
+    res.send('Hello ' + process.env.USER_ID + ' from Node.js!');
+})
+
+app.get('/hello/v2', (req, res) => {
+    res.send('Hello ' + process.env.APP_OWNER + ' from Node.js!');
 })
 
 app.listen(port, () => {
