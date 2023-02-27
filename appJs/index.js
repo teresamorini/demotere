fd630d020d4b732933d58239e620b36091de8032
teresamorini <teresa.morini91@gmail.com>
2023-02-27 18:58:13 +0100

    config map and volumes

--- appJs/index.js
+++ appJs/index.js
@@ -14,6 +14,12 @@ app.get('/hello/v2', (req, res) => {
     res.send('Hello ' + process.env.APP_OWNER + ' from Node.js!');
 })
 
+app.get('/hello/v3', (req, res) => {
+    var PropertiesReader = require('properties-reader');
+    var properties = PropertiesReader('./config/app.properties');
+    res.send('Hello ' + properties.get("APP_OWNER") + ' from Node.js!');
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
